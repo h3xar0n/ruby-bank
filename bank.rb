4759967f0259b45b11cdf3a19d325e0061e79e1e
h3xar0n <aron@glambyadelina.com>
2017-04-27 22:24:17 -0400

    Private methods for PIN and error

--- bank.rb
+++ bank.rb
@@ -1,9 +1,19 @@
-class Account
+class Account 
   attr_reader :name
   attr_reader :balance
     
   def initialize(name, balance=100)
     @balance = balance
     @name = name
-  end  
+  end
+  
+  private
+  
+  def pin
+    @pin = 1234
+  end
+  
+  def pin_error
+    puts "Access denied: incorrect PIN."
+  end
 end
